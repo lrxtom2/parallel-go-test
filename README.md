43d108c348c8bd19a1a426e373842a67301aba34
Su Shi <1684739+metacpp@users.noreply.github.com>
2017-12-14 23:02:02 -0800

    Refactor the forked teamcity-go-test to parallel-go-test.

--- README.md
+++ README.md
@@ -1,14 +1,12 @@
-#TeamCity Go Test Runner
+#Parallel Go Test Runner
 
-`teamcity-go-test` is a replacement test runner for TeamCity for Go. It is
-heavily inspired by [Pavel Gulbin's work][1] <other repo>, but modified to fit
-our use case, where we have lots of tests which take a long time but are safe
-to run in parallel. The workflow is:
+`parallel-go-test` is a parallel test runner for Go. It is heavily inspired by 
+[James Nugent's work](https://github.com/jen20/teamcity-go-test), but removed
+transformation logic on test results. Make sure the tests are safe to run in
+parallel.
 
-1. Compile a test binary using `go test -c`
-2. Pipe a list of test names, one per line, into `teamcity-go-test` , with the
-   `-test` parameter pointing to the executable. To run tests in parallel, set
-   `-parallelism` to a number greater than 1.
-
-
-[1]: https://github.com/2tvenom/go-test-teamcity
+The workflow is:
+1. Compile a test binary using `go test -c`.
+2. Pipe a list of test names, one per line, into `parallel-go-test` , with the
+   `-f` parameter pointing to the path of executable. To run tests in parallel,
+   set `-p` to a number greater than 1.
--- main.go
+++ main.go
@@ -1,114 +1,110 @@
 package main
 
 import (
-	"bufio"
-	"bytes"
-	"flag"
-	"fmt"
-	"io"
-	"os"
-	"os/exec"
-	"strings"
-	"time"
+  "bufio"
+  "bytes"
+  "flag"
+  "fmt"
+  "io"
+  "log"
+  "os"
+  "os/exec"
+  "strings"
 )
 
 func usage() string {
-	return `Usage:
-	teamcity-go-test -test <binary> [-parallelism n]
+  return `Usage:
+  parallel-go-test -f <binary> [-p n]
 
-	Test names must be listed one per line on stdin.
+  Test names must be listed one per line on stdin.
 `
 }
 
 func main() {
-	testBinary := flag.String("test", "", "executable containing the tests to run")
-	parallelism := flag.Int("parallelism", 1, "number of tests to execute in parallel")
-	flag.Parse()
+  binaryPath := flag.String("f", "", "file path of test package")
+  parallelism := flag.Int("p", 1, "number of tests to execute in parallel")
+  flag.Parse()
 
-	if testBinary == nil || *testBinary == "" {
-		fmt.Fprint(os.Stderr, usage())
-		os.Exit(1)
-	}
+  if binaryPath == nil || *binaryPath == "" {
+    fmt.Fprint(os.Stderr, usage())
+    os.Exit(1)
+  }
 
-	if _, err := os.Stat(*testBinary); err != nil {
-		fmt.Fprintf(os.Stderr, "Cannot find binary: %s\n", *testBinary)
-		os.Exit(1)
-	}
+  if _, err := os.Stat(*binaryPath); err != nil {
+    fmt.Fprintf(os.Stderr, "Not valid file path: %s\n", *binaryPath)
+    os.Exit(1)
+  }
 
-	testNames := make([]string, 0, 0)
-	stdInReader := bufio.NewReader(os.Stdin)
+  testNames := make([]string, 0, 0)
+  stdInReader := bufio.NewReader(os.Stdin)
 
-	for {
-		line, err := stdInReader.ReadString('\n')
-		if err != nil {
-			if err == io.EOF {
-				if strings.TrimSpace(line) != "" {
-					testNames = append(testNames, line)
-				}
-				break
-			}
-			fmt.Fprintf(os.Stderr, "error reading stdin: %s", err)
-			os.Exit(1)
-		}
+  for {
+    line, err := stdInReader.ReadString('\n')
+    if err != nil {
+      if err == io.EOF {
+        if strings.TrimSpace(line) != "" {
+          testNames = append(testNames, line)
+        }
+        break
+      }
+      fmt.Fprintf(os.Stderr, "error reading stdin: %s", err)
+      os.Exit(1)
+    }
 
-		if strings.TrimSpace(line) != "" {
-			testNames = append(testNames, line)
-		}
-	}
+    if strings.TrimSpace(line) != "" {
+      testNames = append(testNames, line)
+    }
+  }
 
-	testQueue := make(chan string)
-	messages := make(chan string)
-	completed := make(chan struct{})
+  testQueue := make(chan string)
+  messages := make(chan string)
+  completed := make(chan struct{})
 
-	for i := 0; i < *parallelism; i++ {
-		go runWorker(testQueue, messages, completed, *testBinary)
-	}
+  for i := 0; i < *parallelism; i++ {
+    go runWorker(testQueue, messages, completed, *binaryPath)
+  }
 
-	go func() {
-		for _, testName := range testNames {
-			testQueue <- strings.TrimSpace(testName)
-		}
-	}()
+  go func() {
+    for _, testName := range testNames {
+      testQueue <- strings.TrimSpace(testName)
+    }
+  }()
 
-	resultsCount := 0
-	for {
-		select {
-		case message := <-messages:
-			fmt.Printf("%s", message)
-		case <-completed:
-			resultsCount++
-		}
+  resultsCount := 0
+  for {
+    select {
+    case message := <-messages:
+      fmt.Printf("%s", message)
+    case <-completed:
+      resultsCount++
+    }
 
-		if resultsCount == len(testNames) {
-			break
-		}
-	}
+    if resultsCount == len(testNames) {
+      break
+    }
+  }
 }
 
-func runWorker(inputQueue <-chan string, messages chan<- string, done chan<- struct{}, binaryName string) {
-	for {
-		select {
-		case testName := <-inputQueue:
-			test := NewTeamCityTest(testName)
-			//messages <- fmt.Sprintf("%s", test.FormatStartNotice())
-			runTest(test, binaryName)
-			messages <- test.FormatTestOutput()
-			done <- struct{}{}
-		}
-	}
+func runWorker(inputQueue <-chan string, messages chan<- string,
+               done chan<- struct{}, binaryName string) {
+  for {
+    select {
+    case testName := <-inputQueue:
+      messages <- runTest(testName, binaryName)
+      done <- struct{}{}
+    }
+  }
 }
 
-func runTest(test *TeamCityTest, binaryName string) {
-	var out bytes.Buffer
-	var errOut bytes.Buffer
+func runTest(testName string, binaryName string) string {
+  var stdout = new(bytes.Buffer)
+  cmd := exec.Command(binaryName, "-test.v", "-test.run",
+                      fmt.Sprintf("^%s$", testName), "2>&1")
+  cmd.Stdout = stdout
 
-	test.Started = time.Now()
+  if err := cmd.Run(); err != nil {
+    log.Fatal(err)
+  }
 
-	cmd := exec.Command(binaryName, "-test.v", "-test.run", fmt.Sprintf("^%s$", test.Name))
-	cmd.Stdout = &out
-	cmd.Stderr = &errOut
-	// Not sure what to do with errors here other than report them out to the runner.
-	cmd.Run()
-
-	test.ParseTestRunnerOutput(out.String(), errOut.String())
+  return (*stdout).String()
 }
